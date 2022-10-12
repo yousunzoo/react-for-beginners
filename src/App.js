4e8f9831ec5574533cc3b672ab397c9dd7453d1e
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-12 17:49:09 +0900

    달러 코인 변환기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [invert, setInvert] = useState(1);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -10,20 +13,48 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const onSelect = (event) => {
+    setInvert(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const CoinSelect = () => {
+    return (
+      <select onChange={onSelect} value={invert}>
+        {coins.map((coin) => (
+          <option key={coin.id} value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+    );
+  };
+  const Inverter = () => {
+    return (
+      <div>
+        <label htmlFor="myDollar">Dollar : </label>
+        <input
+          id="myDollar"
+          onChange={onChange}
+          value={amount}
+          type="number"
+          placeholder="write your dollar"
+        />
+        <br />
+        <h3>Invert to</h3>
+        <CoinSelect />
+        <p>{amount / invert}</p>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin, index) => (
-            <option key={index}>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : <Inverter />}
     </div>
   );
 }
